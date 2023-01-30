a77b62285e21d370e6f6a90e8d315a371c214b4f
Ericlis <ericlisfds@hotmail.com>
2023-01-30 05:05:44 -0300

    .

--- pages/category/react.jsx
+++ pages/category/react.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
-import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../../components';
 import { getPosts, getPostsPerCategory } from '../../services'
 
 const react = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, index) => (
